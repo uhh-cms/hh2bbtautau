719d714c8d140681c09a88a2487c01633fd3ece7
Nathan P <prouvost@naf-cms15.desy.de>
2023-05-03 14:31:03 +0200

    revert own changes on files not to be commited

--- hbt/selection/default.py
+++ hbt/selection/default.py
@@ -25,8 +25,6 @@ from hbt.selection.lepton import lepton_selection
 from hbt.selection.jet import jet_selection
 from hbt.production.features import cutflow_features
 
-from IPython import embed
-
 
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
@@ -153,7 +151,6 @@ def default(
     stats: defaultdict,
     **kwargs,
 ) -> tuple[ak.Array, SelectionResult]:
-    embed()
     # ensure coffea behavior
     events = self[attach_coffea_behavior](events, **kwargs)
 
