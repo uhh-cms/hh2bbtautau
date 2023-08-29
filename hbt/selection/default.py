f97a15a5dbda2d1ade2436205711c04a2dd8707e
haddadanas <103462379+haddadanas@users.noreply.github.com>
2023-08-29 17:31:47 +0200

    Update default.py

--- hbt/selection/default.py
+++ hbt/selection/default.py
@@ -92,7 +92,7 @@ def default(
         events = self[pu_weight](events, **kwargs)
 
         # btag weights
-        events = self[btag_weights](events, 
+        events = self[btag_weights](events,
                                     ak.fill_none(results.x.jet_mask, False, axis=1), **kwargs)
 
     # combined event selection after all steps
