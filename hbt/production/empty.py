b1da7ce7e85e77310ce5001349ab42cddb341b66
Bogdan Wiederspan <b.wiederspan@web.de>
2024-01-31 14:26:29 +0100

    fix linting issue in empty produer

--- hbt/production/empty.py
+++ hbt/production/empty.py
@@ -7,14 +7,7 @@ Wrappers for some default sets of producers.
 from columnflow.production import Producer, producer
 from columnflow.production.normalization import normalization_weights
 from columnflow.production.categories import category_ids
-from columnflow.production.cms.electron import electron_weights
-from columnflow.production.cms.muon import muon_weights
 from columnflow.util import maybe_import
-
-from hbt.production.features import features
-from hbt.production.weights import normalized_pu_weight, normalized_pdf_weight, normalized_murmuf_weight
-from hbt.production.btag import normalized_btag_weights
-from hbt.production.tau import tau_weights, trigger_weights
 
 
 ak = maybe_import("awkward")
