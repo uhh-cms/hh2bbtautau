29256d510353774f4cacaac51d824146dba92b76
Ana A <ana.rita.alves.andrade@cern.ch>
2024-05-06 16:56:49 +0200

    removing default calibrations

--- hbt/calibration/placeholder.py
+++ hbt/calibration/placeholder.py
@@ -5,31 +5,20 @@ Placeholder calibrator to produce missing single trigger columns. Includes all d
 """
 
 from columnflow.calibration import Calibrator, calibrator
-from columnflow.calibration.cms.met import met_phi
-from columnflow.calibration.cms.jets import jec, jer
 from columnflow.production.cms.mc_weight import mc_weight
 from columnflow.production.cms.seeds import deterministic_seeds
 from columnflow.util import maybe_import
 from columnflow.columnar_util import set_ak_column
 
-from hbt.calibration.tau import tec
-
-
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 
-
-# derive calibrators to add settings
-jec_nominal = jec.derive("jec_nominal", cls_dict={"uncertainty_sources": [], "data_only": True})
-jec_full = jec.derive("jec_nominal", cls_dict={"mc_only": True})
-
-
 @calibrator(
     uses={
-        mc_weight, jec_nominal, jec_full, jer, tec, deterministic_seeds, met_phi,
+        mc_weight, deterministic_seeds,
     },
     produces={
-        mc_weight, jec_nominal, jec_full, jer, tec, deterministic_seeds, met_phi, "HLT_Ele25_eta2p1_WPTight_Gsf",
+        mc_weight, deterministic_seeds, "HLT_Ele25_eta2p1_WPTight_Gsf",
         "HLT_IsoMu22", "HLT_IsoMu22_eta2p1", "HLT_IsoTkMu22", "HLT_IsoTkMu22_eta2p1",
     },
 )
@@ -38,17 +27,6 @@ def placeholder(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
         events = self[mc_weight](events, **kwargs)
 
     events = self[deterministic_seeds](events, **kwargs)
-
-    if self.dataset_inst.is_data:
-        events = self[jec_nominal](events, **kwargs)
-    else:
-        events = self[jec_full](events, **kwargs)
-        events = self[jer](events, **kwargs)
-
-    events = self[met_phi](events, **kwargs)
-
-    if self.dataset_inst.is_mc:
-        events = self[tec](events, **kwargs)
 
     HLT_Ele25_eta2p1_WPTight_Gsf = ak.any(
         (events.Electron.pt >= 25.5) &
@@ -75,10 +53,3 @@ def placeholder(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
 
     return events
 
-
-@calibrator(
-    uses=set(),
-    produces=set(),
-)
-def empty(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
-    return events
