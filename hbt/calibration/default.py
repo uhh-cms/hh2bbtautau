277e5a2c28088d7b150b72d852c08b718abf686f
Anas <103462379+haddadanas@users.noreply.github.com>
2024-06-07 19:03:26 +0200

    adjusted the calibration for dataset variations + split tec in tec_full and tec_nominal (#23)
    * adjusted the calibration for dataset variations + split tec in tec_full and tec_nominal
    
    * set cf to current master

--- hbt/calibration/default.py
+++ hbt/calibration/default.py
@@ -6,30 +6,29 @@ Calibration methods.
 
 from columnflow.calibration import Calibrator, calibrator
 from columnflow.calibration.cms.met import met_phi
-from columnflow.calibration.cms.jets import jec, jer
+from columnflow.calibration.cms.jets import jec, jec_nominal, jer
 from columnflow.production.cms.mc_weight import mc_weight
 from columnflow.production.cms.seeds import deterministic_seeds
 from columnflow.util import maybe_import
 
 from hbt.util import IF_RUN_2
-from hbt.calibration.tau import tec
+from hbt.calibration.tau import tec, tec_nominal
 
 
 ak = maybe_import("awkward")
 
-
 # derive calibrators to add settings
-jec_nominal = jec.derive("jec_nominal", cls_dict={"uncertainty_sources": [], "data_only": True})
-jec_full = jec.derive("jec_nominal", cls_dict={"mc_only": True})
+jec_full = jec.derive("jec_full", cls_dict={"mc_only": True, "nominal_only": True})
+tec_full = tec.derive("tec_full", cls_dict={"nominal_only": True})
 
 
 @calibrator(
     uses={
-        mc_weight, jec_nominal, jec_full, jer, tec, deterministic_seeds,
+        mc_weight, jec_nominal, jec_full, jer, tec_nominal, tec_full, deterministic_seeds,
         IF_RUN_2(met_phi),
     },
     produces={
-        mc_weight, jec_nominal, jec_full, jer, tec, deterministic_seeds,
+        mc_weight, jec_nominal, jec_full, jer, tec_nominal, tec_full, deterministic_seeds,
         IF_RUN_2(met_phi),
     },
 )
@@ -38,8 +37,7 @@ def default(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
         events = self[mc_weight](events, **kwargs)
 
     events = self[deterministic_seeds](events, **kwargs)
-
-    if self.dataset_inst.is_data:
+    if self.dataset_inst.is_data or not self.global_shift_inst.is_nominal:
         events = self[jec_nominal](events, **kwargs)
     else:
         events = self[jec_full](events, **kwargs)
@@ -49,6 +47,9 @@ def default(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
         events = self[met_phi](events, **kwargs)
 
     if self.dataset_inst.is_mc:
-        events = self[tec](events, **kwargs)
+        if self.global_shift_inst.is_nominal:
+            events = self[tec_full](events, **kwargs)
+        else:
+            events = self[tec_nominal](events, **kwargs)
 
     return events
