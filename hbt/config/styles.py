a03c8709d657c7d7f6722e7b86136b945e1cc320
Marcel R. <github.riga@icloud.com>
2024-06-12 18:07:12 +0200

    Update colors.

--- hbt/config/styles.py
+++ hbt/config/styles.py
@@ -11,8 +11,11 @@ def stylize_processes(config: od.Config) -> None:
     """
     Adds process colors and adjust labels.
     """
-    if config.has_process("hh_ggf_bbtautau"):
-        config.processes.n.hh_ggf_bbtautau.color1 = (67, 118, 201)
+    if config.has_process("hh_ggf_hbb_htt_kl1_kt1"):
+        config.processes.n.hh_ggf_hbb_htt_kl1_kt1.color1 = (67, 118, 201)
+
+    if config.has_process("hh_vbf_hbb_htt_kv1_k2v1_kl1"):
+        config.processes.n.hh_vbf_hbb_htt_kv1_k2v1_kl1.color1 = (86, 211, 71)
 
     if config.has_process("h"):
         config.processes.n.h.color1 = (65, 180, 219)
