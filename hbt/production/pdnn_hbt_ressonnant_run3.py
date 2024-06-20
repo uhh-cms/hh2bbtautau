a1a664cf5e58591600b0b8e93665188c604fae58
Bogdan Wiederspan <b.wiederspan@web.de>
2024-06-20 16:35:53 +0200

    refactor

--- hbt/production/pdnn_hbt_ressonnant_run3.py
+++ hbt/production/pdnn_hbt_ressonnant_run3.py
@@ -15,12 +15,12 @@ from columnflow.columnar_util import set_ak_column
 from columnflow.columnar_util import EMPTY_FLOAT
 from columnflow.util import DotDict
 
-
 np = maybe_import("numpy")
 ak = maybe_import("awkward")
 tf = maybe_import("tensorflow")
 
 logger = law.logger.get_logger(__name__)
+
 
 class Era(enum.Enum):
     """
@@ -34,6 +34,7 @@ class Era(enum.Enum):
     e2022 = e2018
     e2023 = e2018
     e2024 = e2018
+
 
 # helper function
 set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
@@ -54,6 +55,7 @@ def select_DAU(events):
     # sometimes there are more than 2 particles, but network only expects 2
     dau1, dau2 = daus[:, 0], daus[:, 1]
     return dau1, dau2
+
 
 def select_DAU_decay_mode(events):
     """
@@ -87,7 +89,7 @@ def select_DAU_decay_mode(events):
 def get_rotated_kinematics(
     ref_phi: ak.Array,
     events: ak.Array,
-    ):
+):
     """
     Rotates a momentum vector extracted from *events* in the transverse plane to a reference phi angle *ref_phi*.
     Returns the rotated px and py components in a 2-tuple.
@@ -116,17 +118,21 @@ def get_px(events: ak.Array):
     # p_x = p_t cos(phi)
     return events.pt * np.cos(events.phi, dtype=np.float32)
 
+
 def get_py(events: ak.Array):
     # p_y = p_t sin(phi)
     return events.pt * np.sin(events.phi, dtype=np.float32)
+
 
 def get_pz(events: ak.Array):
     # p_z = p_t sinh(eta)
     return events.pt * np.sinh(events.eta, dtype=np.float32)
 
+
 def get_e(px: ak.Array, py: ak.Array, pz: ak.Array, mass: ak.Array):
     # E = sqrt(px^2 + py^2 + pz^2 + m^2)
-    return np.sqrt(px**2 + py**2 + pz**2 + mass**2,)
+    return np.sqrt(px**2 + py**2 + pz**2 + mass**2)
+
 
 def mask_network_unknown_embedding(events, raise_only=False):
     """
@@ -155,7 +161,7 @@ def mask_network_unknown_embedding(events, raise_only=False):
 
         logger.info(
             f"{feature} has  unknown values to embeding: {unique_different_values}"
-            f"expected only {expected_values}."
+            f"expected only {expected_values}.",
         )
         if raise_only:
             continue
@@ -173,70 +179,47 @@ def mask_network_unknown_embedding(events, raise_only=False):
     event_mask = ak.all(masks, axis=0)
     return event_mask
 
+
 @producer(
     uses={
         # custom columns created upstream, probably by a selector
         "channel_id",
         # nano columns
         "event",
-        "Jet.pt", "Jet.eta", "Jet.phi", "Jet.mass",
-        "Jet.btagDeepFlavB", "Jet.btagPNetB",
-        "Jet.btagDeepFlavCvB" , "Jet.btagDeepFlavCvL",
-        "Jet.btagPNetCvB", "Jet.btagPNetCvL", "Jet.btagPNetQvG",
+        "Jet.{pt,eta,phi,mass}",
+        "Jet.btagDeepFlav{B,CvB,CvL}",
+        "Jet.btagPNet{B,CvB,CvL,QvG}",
         "Jet.hhbtag",
-        "MET.pt", "MET.phi",
-        "MET.covXX", "MET.covXY", "MET.covYY",
-        "Electron.*", "Tau.*", "Muon.*",
-        "FatJet.*",
-
+        "MET.{pt,phi}", "MET.cov{XX,XY,YY}",
+        "Electron.{eta,phi,pt,mass,charge}",
+        "Tau.{eta,phi,pt,mass,decayMode,charge}",
+        "Muon.{eta,phi,pt,mass,charge}",
+        "FatJet.{eta,phi,pt,mass}",
     },
     sandbox=dev_sandbox("bash::$HBT_BASE/sandboxes/venv_columnar_tf.sh"),
     produces={
         "ressonant_combined_network_output",
     },
+    # kwargs passed to the parametrized network
+    mass=250,
+    year=3,
+    spin=2,
 )
-def tautauNN_Run3(
+def pdnn_hbt_ressonnant_run3(
     self: Producer,
     events: ak.Array,
     **kwargs,
 ) -> ak.Array:
+    """
+    The producer for the combined multiclass classfifer network used by HBT resonnant analysis.
+    The output scores are classifying if incoming events are HH, Drell-Yan or ttbar.
+    The network uses continous, categorical and parametrized inputs.
+    The network is parametrized in *year*, *spin*, *mass*.
+    Since the network was trained on Run2 data, Run3 uses 2018 as parameter.
 
-    # neural network inputs in the correct order
-    # input_features defined at https://github.com/uhh-cms/tautauNN/blob/f1ca194/evaluation/interface.py#L67
-
-    # get user input information about year, spin and mass (parametrized network)
-    self.mass = 700
-    self.year = 3
-    self.spin = 2
-
-
-    # input_features defined at: https://github.com/uhh-cms/tautauNN/blob/main/tautaunn/config.py#L512
-    contionus_input_features = [
-        # MET kinematics, DeepMETResponseTune and covariance matrix
-        "met_px", "met_py", "met_cov00", "met_cov01", "met_cov11",
-        # DAU kinematics
-        "dau1_e", "dau1_px", "dau1_py", "dau1_pz",
-        "dau2_e", "dau2_px", "dau2_py", "dau2_pz",
-        # BJET kinematics
-        "bjet1_e", "bjet1_px", "bjet1_py", "bjet1_pz", "bjet2_e", "bjet2_px", "bjet2_py", "bjet2_pz",
-        # BTAG scores, pnet and hhbtag
-        "bjet1_btag_df", "bjet1_cvsb", "bjet1_cvsl", "bjet1_hhbtag",
-        "bjet2_btag_df", "bjet2_cvsb", "bjet2_cvsl", "bjet2_hhbtag",
-        # fat jet kinematics
-        "fatjet_e", "fatjet_px", "fatjet_py", "fatjet_pz",    ]
-
-    categorical_input_features = [
-        # channel_id, 0=muon_tau, 1=electron_tau, 2=tau_tau
-        "pair_type",
-        # dau decay mode, -1 for lepton, 0 for 1-prong, 1 for 3-prong, 10 for 1-prong+pi0, 11 for 3-prong+pi0
-        "dau1_dm", "dau2_dm", "dau1_charge", "dau2_charge",
-        # any additional flags
-        "is_boosted", "has_bjet_pair",
-    ]
-
-    categorical_targets = [
-        "HH", "Drell-Yan", "ttbar"
-    ]
+    A list of all inputs in the correct order can be found in the tautauNN repo:
+    https://github.com/uhh-cms/tautauNN/blob/f1ca194/evaluation/interface.py#L67
+    """
 
     # prepare events for network
     f = DotDict()
@@ -253,7 +236,7 @@ def tautauNN_Run3(
     phi_lep = np.arctan2(
         f.dau1_py + f.dau2_py,
         f.dau1_px + f.dau2_px,
-                    )
+    )
 
     f.dau1_px, f.dau1_py = get_rotated_kinematics(phi_lep, dau1)
     f.dau1_pz = get_pz(dau1)
@@ -309,14 +292,6 @@ def tautauNN_Run3(
         "bjet2_btag_df": - 1.0,
         "bjet2_cvsb": - 1.0,
         "bjet2_cvsl": - 1.0,
-        # "hbb_e": 0.0,
-        # "hbb_px": 0.0,
-        # "hbb_py": 0.0,
-        # "hbb_pz": 0.0,
-        # "htthbb_e": 0.0,
-        # "htthbb_px": 0.0,
-        # "htthbb_py": 0.0,
-        # "htthbb_pz": 0.0,
         "fatjet_e": 0.0,
         "fatjet_px": 0.0,
         "fatjet_py": 0.0,
@@ -376,34 +351,25 @@ def tautauNN_Run3(
     f.dau1_charge, f.dau2_charge = dau1.charge, dau2.charge
 
     # has_bjet_pair
-    # defined in https://github.com/uhh-cms/tautauNN/blob/5c5371bc852f2a5c0a40ce0ecd9ffaa0c360e565/tautaunn/config.py#L632
+    # see: https://github.com/uhh-cms/tautauNN/blob/5c5371bc852f2a5c0a40ce0ecd9ffaa0c360e565/tautaunn/config.py#L632
     f.has_bjet_pair = ak.num(events.Jet) > 1
 
     # parametrized features
     # fill constant value for mass, year and spin
     f.mass = ak.full_like(f.met_px, self.mass, dtype=np.int32)
-    f.year = ak.full_like(f.met_px, self.year, dtype=np.int32)
+    f.year = ak.full_like(f.met_px, Era[self.year], dtype=np.int32)
     f.spin = ak.full_like(f.met_px, self.spin, dtype=np.int32)
 
     # filter out events with bad embedding, if raise_only=True only log the events, but do not mask
     selection_mask = mask_network_unknown_embedding(f, raise_only=False)
-    # these masks are in the interface but not used right now
-    # bj_mask = ~f.has_bjet_pair == 1
-    # f.bjet1_e[bj_mask] = f.bjet1_px[bj_mask] = f.bjet1_py[bj_mask] = f.bjet1_pz[bj_mask] = 0.0  # noqa
-    # f.bjet2_e[bj_mask] = f.bjet2_px[bj_mask] = f.bjet2_py[bj_mask] = f.bjet2_pz[bj_mask] = 0.0  # noqa
-    # f.bjet1_btag_df[bj_mask] = f.bjet1_cvsb[bj_mask] = f.bjet1_cvsl[bj_mask] = -1.0
-    # f.bjet2_btag_df[bj_mask] = f.bjet2_cvsb[bj_mask] = f.bjet2_cvsl[bj_mask] = -1.0
-    # f.hbb_e[bj_mask] = f.hbb_px[bj_mask] = f.hbb_py[bj_mask] = f.hbb_pz[bj_mask] = 0.0  # noqa
-    # f.htthbb_e[bj_mask] = f.htthbb_px[bj_mask] = f.htthbb_py[bj_mask] = f.htthbb_pz[bj_mask] = 0.0  # noqa
 
-    # # mask fatjet features when there was actually no fatjet
-    # fj_mask = ~f.is_boosted == 1
-    # f.fatjet_e[fj_mask] = f.fatjet_px[fj_mask] = f.fatjet_py[fj_mask] = f.fatjet_pz[fj_mask] = 0.0  # noqa
+    # convert the networks input into a tensorflow tensor and evaluate the model
+    # taken from https://github.com/uhh-cms/tautauNN/blob/f1ca194/evaluation/interface.py#L159
+    categorical_targets = [
+        "HH", "Drell-Yan", "ttbar",
+    ]
 
-    # convert everything into tensorflow tensors
-    # ak.local_index(event_mask)[event_mask]
-    # build input tensors
-    cont_inputs = tf.concat(
+    continous_inputs = tf.concat(
         [t[..., None] for t in [
             f.met_px, f.met_py, f.met_cov00, f.met_cov01, f.met_cov11,
             f.dau1_px, f.dau1_py, f.dau1_pz, f.dau1_e,
@@ -420,7 +386,7 @@ def tautauNN_Run3(
         axis=1,
     )
 
-    cat_inputs = tf.concat(
+    categorical_inputs = tf.concat(
         [t[..., None] for t in [
             f.pair_type, f.dau1_decay_mode, f.dau2_decay_mode,
             f.dau1_charge, f.dau2_charge,
@@ -433,25 +399,23 @@ def tautauNN_Run3(
     # evaluate the model
     model = self.tautauNN_model.signatures["serving_default"]
 
-    # use the network
     # mask events that have bad embedding
     masked_network_inputs = {
-        "cat_input": tf.cast(tf.boolean_mask(cat_inputs, selection_mask), np.int32),
-        "cont_input": tf.cast(tf.boolean_mask(cont_inputs, selection_mask), np.float32),
+        "cat_input": tf.cast(tf.boolean_mask(categorical_inputs, selection_mask), np.int32),
+        "cont_input": tf.cast(tf.boolean_mask(continous_inputs, selection_mask), np.float32),
     }
-
     network_scores = model(**masked_network_inputs)
+
     # fill the scores with EMPTY_FLOAT for events that were masked
+    # place the scores in the correct column
     classification_scores = np.ones((len(events), len(categorical_targets)), dtype=np.float32) * EMPTY_FLOAT
     classification_scores[selection_mask] = network_scores["hbt_ensemble"].numpy()
-    from IPython import embed; embed()
     events = set_ak_column_f32(events, "ressonant_combined_network_output", classification_scores)
-
     return events
 
 
-@tautauNN_Run3.requires
-def tautauNN_Run3_requires(self: Producer, reqs: dict) -> None:
+@pdnn_hbt_ressonnant_run3.requires
+def pdnn_hbt_ressonnant_run3_requires(self: Producer, reqs: dict) -> None:
     """
     Add the external files bundle to requirements.
     """
@@ -462,8 +426,8 @@ def tautauNN_Run3_requires(self: Producer, reqs: dict) -> None:
     reqs["external_files"] = BundleExternalFiles.req(self.task)
 
 
-@tautauNN_Run3.setup
-def tautauNN_Run3_setup(self: Producer, reqs: dict, inputs: dict, reader_targets: InsertableDict) -> None:
+@pdnn_hbt_ressonnant_run3.setup
+def pdnn_hbt_ressonnant_run3_setup(self: Producer, reqs: dict, inputs: dict, reader_targets: InsertableDict) -> None:
     """
     Sets up the two tautauNN TF models.
     """
@@ -486,22 +450,3 @@ def tautauNN_Run3_setup(self: Producer, reqs: dict, inputs: dict, reader_targets
             "extended_pair_ED10_LU8x128_CTdense_ACTelu_BNy_LT50_DO0_BS4096_OPadamw_LR1.0e-03"
             "_YEARy_SPINy_MASSy_RSv6_fi80_lbn_ft_lt20_lr1_LBdefault"
             "_daurot_fatjet_composite_FI0_SDx5").path)
-
-
-def apply_tautauNN_Run3_selection_cut(events: ak.Array, **kwargs) -> ak.Array:
-    pre_selection_cut: str | list[str] | dict[str, list[str]] = [
-        "nbjetscand > 1",
-        "nleps == 0",
-        "isOS == 1",
-        "dau2_deepTauVsJet >= 5",
-        (
-            "((pair_type == 0) & (dau1_iso < 0.15) & (isLeptrigger == 1)) | "
-            "((pair_type == 1) & (dau1_eleMVAiso == 1) & (isLeptrigger == 1)) | "
-            "((pair_type == 2) & (dau1_deepTauVsJet >= 5))"
-        ),
-    ],
-
-
-    return events
-
-
