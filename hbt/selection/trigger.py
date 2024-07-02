01568ce239a8d564f37dc8b42f2909bb70bb94a7
Nathan Prouvost <49162277+nprouvost@users.noreply.github.com>
2024-07-02 10:51:02 +0200

    refactoring
    Co-authored-by: Marcel Rieger <riga@users.noreply.github.com>

--- hbt/selection/trigger.py
+++ hbt/selection/trigger.py
@@ -46,13 +46,11 @@ def trigger_selection(
             continue
 
         # get bare decisions
-        if trigger.run_range is None:
-            fired = events.HLT[trigger.hlt_field] == 1
-        else:
-            fired = (
-                (events.HLT[trigger.hlt_field] == 1) &
-                ((trigger.run_range[0] is None) | (trigger.run_range[0] < events.run)) &
-                ((trigger.run_range[1] is None) | (trigger.run_range[1] > events.run))
+        fired = events.HLT[trigger.hlt_field] == 1
+        if trigger.run_range:
+            fired = fired & (
+                ((trigger.run_range[0] is None) | (trigger.run_range[0] <= events.run)) &
+                ((trigger.run_range[1] is None) | (trigger.run_range[1] >= events.run))
             )
         any_fired = any_fired | fired
 
