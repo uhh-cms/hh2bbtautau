48d1d5793d2e8ef65f5aaba7b25b3fb1e7b9df9f
Marcel R. <github.riga@icloud.com>
2024-07-12 16:51:17 +0200

    Use stiched producer.

--- hbt/production/default.py
+++ hbt/production/default.py
@@ -5,14 +5,16 @@ Wrappers for some default sets of producers.
 """
 
 from columnflow.production import Producer, producer
-from columnflow.production.normalization import normalization_weights
+from columnflow.production.normalization import stitched_normalization_weights
 from columnflow.production.categories import category_ids
 from columnflow.production.cms.electron import electron_weights
 from columnflow.production.cms.muon import muon_weights
 from columnflow.util import maybe_import
 
 from hbt.production.features import features
-from hbt.production.weights import normalized_pu_weight, normalized_pdf_weight, normalized_murmuf_weight
+from hbt.production.weights import (
+    normalized_pu_weight, normalized_pdf_weight, normalized_murmuf_weight,
+)
 from hbt.production.btag import normalized_btag_weights
 from hbt.production.tau import tau_weights, trigger_weights
 from hbt.util import IF_DATASET_HAS_LHE_WEIGHTS
@@ -23,12 +25,12 @@ ak = maybe_import("awkward")
 
 @producer(
     uses={
-        category_ids, features, normalization_weights, normalized_pu_weight,
+        category_ids, features, stitched_normalization_weights, normalized_pu_weight,
         normalized_btag_weights, tau_weights, electron_weights, muon_weights, trigger_weights,
         IF_DATASET_HAS_LHE_WEIGHTS(normalized_pdf_weight, normalized_murmuf_weight),
     },
     produces={
-        category_ids, features, normalization_weights, normalized_pu_weight,
+        category_ids, features, stitched_normalization_weights, normalized_pu_weight,
         normalized_btag_weights, tau_weights, electron_weights, muon_weights, trigger_weights,
         IF_DATASET_HAS_LHE_WEIGHTS(normalized_pdf_weight, normalized_murmuf_weight),
     },
@@ -43,7 +45,7 @@ def default(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     # mc-only weights
     if self.dataset_inst.is_mc:
         # normalization weights
-        events = self[normalization_weights](events, **kwargs)
+        events = self[stitched_normalization_weights](events, **kwargs)
 
         # normalized pdf weight
         if self.has_dep(normalized_pdf_weight):
