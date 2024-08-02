21c0db26a2d326caf23134073bc1b65de6b615c2
Ana A <ana.rita.alves.andrade@cern.ch>
2024-08-02 14:35:27 +0200

    test: add 2j cat and shape uncertanties

--- hbt/inference/inf_model.py
+++ hbt/inference/inf_model.py
@@ -1,12 +1,21 @@
-from columnflow.inference import inference_model, ParameterType
+from columnflow.inference import inference_model, ParameterType, ParameterTransformation
 
 
 @inference_model
 def test_unc(self):
     self.add_category(
-        "channel",
+        "incl",
         config_category="incl",
         config_variable="hh_mass",
+        # fake data
+        data_from_processes=["TT", "dy"],
+        mc_stats=True,
+    )
+
+    self.add_category(
+        "2j",
+        config_category="2j",
+        config_variable="jet1_pt",
         # fake data
         data_from_processes=["TT", "dy"],
         mc_stats=True,
@@ -49,8 +58,17 @@ def test_unc(self):
         )
         self.add_parameter_to_group(unc_name, "experiment")
 
+    # electron uncertainty
+    self.add_parameter(
+        "CMS_eff_e",  # this is the name of the uncertainty as it will show in the datacard. Let's use some variant of the official naming
+        process="*",
+        type=ParameterType.shape,
+        config_shift_source="e",  # this is the name of the shift (alias) in the config
+    )
+    self.add_parameter_to_group("CMS_eff_e", "experiment")
+
+
     # a custom asymmetric uncertainty
-    """
     self.add_parameter(
         "QCDscale_ttbar",
         process="TT",
@@ -60,6 +78,8 @@ def test_unc(self):
     )
     self.add_parameter_to_group("QCDscale_ttbar", "experiment")
 
+
+    """
     # tune uncertainty
     self.add_parameter(
         "tune",
