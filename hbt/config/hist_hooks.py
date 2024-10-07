b0eefa7a846ab1cc986453777c611711dfe41271
Ana A <ana.rita.alves.andrade@cern.ch>
2024-10-07 14:51:27 +0200

    adding hooks for transfer factor calculations

--- hbt/config/hist_hooks.py
+++ hbt/config/hist_hooks.py
@@ -187,32 +187,7 @@ def add_hist_hooks(config: od.Config) -> None:
 
         return hists
 
-    def avoid_plotting(task, hists):
-        cats = [
-            task.config_inst.get_category(c)
-            for c in [f"incl__{a}__{b}" for a in ["os", "ss"] for b in ["iso", "noniso"]]
-        ]
-
-        results = {}
-        for process, h in hists.items():
-            h_new = hist.Hist(
-                hist.axes.StrCategory([c.name for c in cats], name=h.axes[-1].name),
-                hist.axes.IntCategory([0], name="shift"),
-                hist.axes.IntCategory([101], name="category"),
-                storage=hist.storage.Weight(),
-            )
-            for ind, big_cat in enumerate(cats):
-                h_sum = h[
-                    {
-                        "category": [hist.loc(cat.id) for cat in big_cat.get_leaf_categories()],
-                        "shift": sum,
-                    }
-                ].sum()
-                h_new.values()[ind][0][0] = h_sum.value
-                h_new.variances()[ind][0][0] = h_sum.variance
-            results[process] = h_new
-        return results
-
+    # calculate the transfer factor for a chosen category and single decay channel (etau OR mutau OR tautau)
     def fake_factor(task, hists):
         if not hists:
             return hists
@@ -316,8 +291,132 @@ def add_hist_hooks(config: od.Config) -> None:
                 )
         return hists
 
+    # calculate the transfer factor for a chosen category for all decay channels (etau AND muta AND tautau)
+    def fake_factor_incl(task, hists):
+        if not hists:
+            return hists
+
+        # get dummy processes
+        factor_bin = config.get_process("qcd", default=None)
+        if not factor_bin:
+            return hists
+
+        factor_int = config.get_process("dy", default=None)
+        if not factor_int:
+            return hists
+
+        # extract all unique category ids and verify that the axis order is exactly
+        # "category -> shift -> variable" which is needed to insert values at the end
+        CAT_AXIS, SHIFT_AXIS, VAR_AXIS = range(3)
+        category_ids = set()
+        for proc, h in hists.items():
+            # validate axes
+            assert len(h.axes) == 3
+            assert h.axes[CAT_AXIS].name == "category"
+            assert h.axes[SHIFT_AXIS].name == "shift"
+            # get the category axis
+            cat_ax = h.axes["category"]
+            for cat_index in range(cat_ax.size):
+                category_ids.add(cat_ax.value(cat_index))
+
+        # create qcd groups
+        qcd_groups: dict[str, dict[str, od.Category]] = defaultdict(DotDict)
+        for cat_id in category_ids:
+            cat_inst = config.get_category(cat_id)
+            if cat_inst.has_tag({"os", "iso"}, mode=all):
+                qcd_groups[cat_inst.x.qcd_group].os_iso = cat_inst
+            elif cat_inst.has_tag({"os", "noniso"}, mode=all):
+                qcd_groups[cat_inst.x.qcd_group].os_noniso = cat_inst
+            elif cat_inst.has_tag({"ss", "iso"}, mode=all):
+                qcd_groups[cat_inst.x.qcd_group].ss_iso = cat_inst
+            elif cat_inst.has_tag({"ss", "noniso"}, mode=all):
+                qcd_groups[cat_inst.x.qcd_group].ss_noniso = cat_inst
+
+        # get complete qcd groups
+        complete_groups = [name for name, cats in qcd_groups.items() if len(cats) == 4]
+
+        # nothing to do if there are no complete groups
+        if not complete_groups:
+            return hists
+
+        # sum up mc and data histograms, stop early when empty
+        mc_hists = [h for p, h in hists.items() if p.is_mc and not p.has_tag("signal")]
+        data_hists = [h for p, h in hists.items() if p.is_data]
+        if not mc_hists or not data_hists:
+            return hists
+        mc_hist = sum(mc_hists[1:], mc_hists[0].copy())
+        data_hist = sum(data_hists[1:], data_hists[0].copy())
+
+        # start by copying the mc hist and reset it, then fill it at specific category slices
+        hists = {}
+        channels = {}
+        hists[factor_bin] = factor_hist = mc_hist.copy().reset()
+        hists[factor_int] = factor_hist_int = mc_hist.copy().reset()
+
+        for group_name in complete_groups:
+            group = qcd_groups[group_name]
+            # get the corresponding histograms and convert them to number objects,
+            # each one storing an array of values with uncertainties
+            # shapes: (SHIFT, VAR)
+            get_hist = lambda h, region_name: h[{"category": hist.loc(group[region_name].id)}]
+            ss_noniso_mc = hist_to_num(get_hist(mc_hist, "ss_noniso"), "ss_noniso_mc")
+            ss_iso_mc = hist_to_num(get_hist(mc_hist, "ss_iso"), "ss_iso_mc")
+            ss_noniso_data = hist_to_num(get_hist(data_hist, "ss_noniso"), "ss_noniso_data")
+            ss_iso_data = hist_to_num(get_hist(data_hist, "ss_iso"), "ss_iso_data")
+
+            channels[group_name] = {}
+            channels[group_name]["ss_iso_mc"] = ss_iso_mc
+            channels[group_name]["ss_noniso_mc"] = ss_noniso_mc
+            channels[group_name]["ss_iso_data"] = ss_iso_data
+            channels[group_name]["ss_noniso_data"] = ss_noniso_data
+
+        for group_name in complete_groups:
+            for k in ["incl"]:  # INDICATE WHICH CATEGORY TO CALCULATE THE FACTOR FOR ! e.g. "incl", "2j" ...
+                if group_name == f"etau__{k}":  # DUMMY CATEGORY! mutau and tautau categories will have no factor calculated and etau is a proxy for the incl/2j/... category chosen
+                    group = qcd_groups[group_name]
+
+                    ss_iso_mc = channels[f"etau__{k}"]["ss_iso_mc"] + channels[f"mutau__{k}"]["ss_iso_mc"] + channels[f"tautau__{k}"]["ss_iso_mc"]
+                    ss_noniso_mc = channels[f"etau__{k}"]["ss_noniso_mc"] + channels[f"mutau__{k}"]["ss_noniso_mc"] + channels[f"tautau__{k}"]["ss_noniso_mc"]
+                    ss_iso_data = channels[f"etau__{k}"]["ss_iso_data"] + channels[f"mutau__{k}"]["ss_iso_data"] + channels[f"tautau__{k}"]["ss_iso_data"]
+                    ss_noniso_data = channels[f"etau__{k}"]["ss_noniso_data"] + channels[f"mutau__{k}"]["ss_noniso_data"] + channels[f"tautau__{k}"]["ss_noniso_data"]
+
+                    # take the difference between data and MC in the control regions
+                    ss_iso_qcd = ss_iso_data - ss_iso_mc
+                    ss_noniso_qcd = ss_noniso_data - ss_noniso_mc
+
+                    # calculate the pt-independent fake factor
+                    int_ss_iso = integrate_num(ss_iso_qcd, axis=1)
+                    int_ss_noniso = integrate_num(ss_noniso_qcd, axis=1)
+                    fake_factor_int = (int_ss_iso / int_ss_noniso)[0, None]
+
+                    # calculate the pt-dependent fake factor
+                    fake_factor = (ss_iso_qcd / ss_noniso_qcd)[:, None]
+                    fake_factor_values = np.squeeze(np.nan_to_num(fake_factor()), axis=0)
+                    fake_factor_variances = fake_factor(sn.UP, sn.ALL, unc=True)**2
+
+                    # change shape of fake_factor_int for plotting
+                    fake_factor_int_values = fake_factor_values.copy()
+                    fake_factor_int_values.fill(fake_factor_int()[0])
+
+                    # insert values into the qcd histogram
+                    cat_axis = factor_hist.axes["category"]
+                    for cat_index in range(cat_axis.size):
+                        if cat_axis.value(cat_index) == group.os_iso.id:
+                            factor_hist.view().value[cat_index, ...] = fake_factor_values
+                            factor_hist.view().variance[cat_index, ...] = fake_factor_variances
+                            factor_hist_int.view().value[cat_index, ...] = fake_factor_int_values
+                            break
+                    else:
+                        raise RuntimeError(
+                            f"could not find index of bin on 'category' axis of qcd histogram {factor_hist} "
+                            f"for category {group.os_iso}",
+                        )
+        from IPython import embed
+        embed()
+        return hists
+
     config.x.hist_hooks = {
         "qcd": qcd_estimation,
-        "avoid_plotting": avoid_plotting,
         "fake_factor": fake_factor,
+        "fake_factor_incl": fake_factor_incl,
     }
