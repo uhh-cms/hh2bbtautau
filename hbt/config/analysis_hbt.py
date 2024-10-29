d9bebbd39467ebf12206bbf466800e5eb65dc9e5
Marcel R. <github.riga@icloud.com>
2024-10-29 17:52:15 +0100

    Add back now completed 22pre config.

--- hbt/config/analysis_hbt.py
+++ hbt/config/analysis_hbt.py
@@ -123,13 +123,13 @@ def add_lazy_config(
 # Run 3 configs
 #
 
-# 2022, preEE, TODO: disabled until re-production of missing datasets is done by Nathan
-# add_lazy_config(
-#     campaign_module="cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12",
-#     campaign_attr="campaign_run3_2022_preEE_nano_uhh_v12",
-#     config_name="run3_2022_preEE",
-#     config_id=5,
-# )
+# 2022, preEE
+add_lazy_config(
+    campaign_module="cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12",
+    campaign_attr="campaign_run3_2022_preEE_nano_uhh_v12",
+    config_name="run3_2022_preEE",
+    config_id=5,
+)
 
 # 2022, postEE
 add_lazy_config(
