cf2dd3a90280f07df919ca91ab67b2ee21c32996
Bogdan Wiederspan <b.wiederspan@web.de>
2025-01-20 17:17:13 +0100

    added dijet dilep dilepjet variables, as well as single lepon variables. added a default variables group to produce create standard controll plots faster.

--- hbt/config/variables.py
+++ hbt/config/variables.py
@@ -3,6 +3,7 @@
 """
 Definition of variables.
 """
+from functools import partial
 
 import order as od
 
@@ -64,7 +65,6 @@ def add_variables(config: od.Config) -> None:
         config,
         name="jet_pt",
         expression="Jet.pt",
-        null_value=EMPTY_FLOAT,
         binning=(40, 0.0, 400.0),
         unit="GeV",
         x_title=r"all Jet $p_{T}$",
@@ -73,7 +73,6 @@ def add_variables(config: od.Config) -> None:
         config,
         name="jet1_pt",
         expression="Jet.pt[:,0]",
-        null_value=EMPTY_FLOAT,
         binning=(40, 0.0, 400.0),
         unit="GeV",
         x_title=r"Jet 1 $p_{T}$",
@@ -82,42 +81,44 @@ def add_variables(config: od.Config) -> None:
         config,
         name="jet1_eta",
         expression="Jet.eta[:,0]",
-        null_value=EMPTY_FLOAT,
         binning=(30, -3.0, 3.0),
         x_title=r"Jet 1 $\eta$",
     )
     add_variable(
         config,
+        name="jet1_phi",
+        expression="Jet.phi[:,0]",
+        binning=(33, -3.3, 3.3),
+        x_title=r"Jet 1 $\phi$",
+    )
+    add_variable(
+        config,
         name="jet2_pt",
         expression="Jet.pt[:,1]",
-        null_value=EMPTY_FLOAT,
         binning=(40, 0.0, 400.0),
         unit="GeV",
         x_title=r"Jet 2 $p_{T}$",
     )
     add_variable(
         config,
+        name="jet2_eta",
+        expression="Jet.eta[:,1]",
+        binning=(30, -3.0, 3.0),
+        x_title=r"Jet 2 $\eta$",
+    )
+    add_variable(
+        config,
+        name="jet2_phi",
+        expression="Jet.phi[:,1]",
+        binning=(33, -3.3, 3.3),
+        x_title=r"Jet 2 $\phi$",
+    )
+    add_variable(
+        config,
         name="met_phi",
         expression="PuppiMET.phi",
-        null_value=EMPTY_FLOAT,
         binning=(33, -3.3, 3.3),
         x_title=r"MET $\phi$",
-    )
-    add_variable(
-        config,
-        name="e1_pt",
-        expression="Electron.pt[:, 0]",
-        null_value=EMPTY_FLOAT,
-        binning=(40, 0, 400),
-        x_title=r"Leading electron p$_{T}$",
-    )
-    add_variable(
-        config,
-        name="mu1_pt",
-        expression="Muon.pt[:,0]",
-        null_value=EMPTY_FLOAT,
-        binning=(40, 0, 400),
-        x_title=r"Leading muon p$_{T}$",
     )
 
     # weights
@@ -215,31 +216,6 @@ def add_variables(config: od.Config) -> None:
     # variables of interest
     add_variable(
         config,
-        name="hh_mass",
-        expression="hh.mass",
-        binning=(20, 250, 750.0),
-        unit="GeV",
-        x_title=r"$m_{hh}$",
-    )
-    add_variable(
-        config,
-        name="hh_pt",
-        expression="hh.pt",
-        binning=(100, 0, 500.0),
-        unit="GeV",
-        x_title=r"$p_T$",
-    )
-    add_variable(
-        config,
-        name="hh_eta",
-        expression="hh.eta",
-        binning=(100, -3.0, 3.0),
-        unit="GeV",
-        x_title=r"$\eta$",
-    )
-
-    add_variable(
-        config,
         name="ditau_mass",
         expression="diTau.mass",
         binning=(20, 50, 200.0),
@@ -277,7 +253,7 @@ def add_variables(config: od.Config) -> None:
         expression="diBJet.pt",
         binning=(100, 0, 500.0),
         unit="GeV",
-        x_title=r"$p_T$",
+        x_title=r"$p_{T}$",
     )
     add_variable(
         config,
@@ -288,22 +264,383 @@ def add_variables(config: od.Config) -> None:
         x_title=r"$\eta$",
     )
 
-    def dilep_mass_test(events):
+    # build variables for dilepton, dijet, and hh
+    def delta_r12(vectors):
+        # delta r between first two elements
+        import awkward as ak
+        dr = ak.firsts(vectors[:, :1], axis=1).delta_r(ak.firsts(vectors[:, 1:2], axis=1))
+        return ak.fill_none(dr, EMPTY_FLOAT)
+
+    def build_dilep(events, which=None):
         import awkward as ak
         leps = ak.concatenate([events.Electron * 1, events.Muon * 1, events.Tau * 1], axis=1)[:, :2]
+        if which == "dr":
+            return delta_r12(leps)
         dilep = leps.sum(axis=1)
-        return dilep.mass
+        if which is None:
+            return dilep * 1
+        if which == "mass":
+            return dilep.mass
+        if which == "pt":
+            return dilep.pt
+        if which == "eta":
+            return dilep.eta
+        if which == "abs_eta":
+            return abs(dilep.eta)
+        if which == "phi":
+            return dilep.phi
+        if which == "energy":
+            return dilep.energy
+        raise ValueError(f"Unknown which: {which}")
 
+    build_dilep.inputs = ["{Electron,Muon,Tau}.{pt,eta,phi,mass}"]
+
+    def build_dijet(events, which=None):
+        import awkward as ak
+        jets = events.Jet[:, :2]
+        if which == "dr":
+            return delta_r12(jets)
+        dijet = jets.sum(axis=1)
+        if which is None:
+            return dijet * 1
+        if which == "mass":
+            return dijet.mass
+        if which == "pt":
+            return dijet.pt
+        if which == "eta":
+            return dijet.eta
+        if which == "abs_eta":
+            return abs(dijet.eta)
+        if which == "phi":
+            return dijet.phi
+        if which == "energy":
+            return dijet.energy
+        raise ValueError(f"Unknown which: {which}")
+
+    build_dijet.inputs = ["Jet.{pt,eta,phi,mass}"]
+
+    def build_hh(events, which=None):
+        import awkward as ak
+        dijet = build_dijet(events)
+        dilep = build_dilep(events)
+        hs = ak.concatenate([dijet[..., None], dilep[..., None]], axis=1)
+        if which == "dr":
+            return delta_r12(hs)
+        hh = hs.sum(axis=1)
+        if which is None:
+            return hh * 1
+        if which == "mass":
+            return hh.mass
+        if which == "pt":
+            return hh.pt
+        if which == "eta":
+            return hh.eta
+        if which == "abs_eta":
+            return abs(hh.eta)
+        if which == "phi":
+            return hh.phi
+        if which == "energy":
+            return hh.energy
+        raise ValueError(f"Unknown which: {which}")
+
+    build_hh.inputs = build_dijet.inputs + build_dilep.inputs
+
+    # dijet variables
+    add_variable(
+        config,
+        name="dijet_energy",
+        expression=partial(build_dijet, which="energy"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(40, 40, 300),
+        unit="GeV",
+        x_title=r"$E_{jj}$",
+    )
+    add_variable(
+        config,
+        name="dijet_mass",
+        expression=partial(build_dijet, which="mass"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(30, 0, 300),
+        unit="GeV",
+        x_title=r"$m_{jj}$",
+    )
+    add_variable(
+        config,
+        name="dijet_pt",
+        expression=partial(build_dijet, which="pt"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(40, 0, 200),
+        unit="GeV",
+        x_title=r"$p_{T,jj}$",
+    )
+    add_variable(
+        config,
+        name="dijet_eta",
+        expression=partial(build_dijet, which="eta"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(50, -5, 5),
+        unit="GeV",
+        x_title=r"$\eta_{jj}$",
+    )
+    add_variable(
+        config,
+        name="dijet_phi",
+        expression=partial(build_dijet, which="phi"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(32, -3.2, 3.2),
+        unit="GeV",
+        x_title=r"$\phi_{jj}$",
+    )
+    add_variable(
+        config,
+        name="dijet_dr",
+        expression=partial(build_dijet, which="dr"),
+        aux={"inputs": build_dijet.inputs},
+        binning=(30, 0, 6),
+        x_title=r"$\Delta R_{jj}$",
+    )
+
+    # dilepton variables
+    add_variable(
+        config,
+        name="dilep_energy",
+        expression=partial(build_dilep, which="energy"),
+        aux={"inputs": build_dilep.inputs},
+        binning=(40, 40, 300),
+        unit="GeV",
+        x_title=r"$E_{ll}$",
+    )
     add_variable(
         config,
         name="dilep_mass",
-        expression=dilep_mass_test,
-        aux={
-            "inputs": ["{Electron,Muon,Tau}.{pt,eta,phi,mass}"],
-        },
+        expression=partial(build_dilep, which="mass"),
+        aux={"inputs": build_dilep.inputs},
         binning=(40, 40, 120),
         unit="GeV",
         x_title=r"$m_{ll}$",
+    )
+    add_variable(
+        config,
+        name="dilep_pt",
+        expression=partial(build_dilep, which="pt"),
+        aux={"inputs": build_dilep.inputs},
+        binning=(40, 0, 200),
+        unit="GeV",
+        x_title=r"$p_{T,ll}$",
+    )
+    add_variable(
+        config,
+        name="dilep_eta",
+        expression=partial(build_dilep, which="eta"),
+        aux={"inputs": build_dilep.inputs},
+        binning=(50, -5, 5),
+        unit="GeV",
+        x_title=r"$\eta_{ll}$",
+    )
+    add_variable(
+        config,
+        name="dilep_phi",
+        expression=partial(build_dilep, which="phi"),
+        aux={"inputs": build_dilep.inputs},
+        binning=(32, -3.2, 3.2),
+        unit="GeV",
+        x_title=r"$\phi_{ll}$",
+    )
+    add_variable(
+        config,
+        name="dilep_dr",
+        expression=partial(build_dilep, which="dr"),
+        aux={"inputs": build_dilep.inputs},
+        binning=(30, 0, 6),
+        x_title=r"$\Delta R_{ll}$",
+    )
+
+    # hh variables
+    add_variable(
+        config,
+        name="hh_energy",
+        expression=partial(build_hh, which="energy"),
+        aux={"inputs": build_hh.inputs},
+        binning=(40, 100, 800),
+        unit="GeV",
+        x_title=r"$E_{ll,jj}$",
+    )
+    add_variable(
+        config,
+        name="hh_mass",
+        expression=partial(build_hh, which="mass"),
+        aux={"inputs": build_hh.inputs},
+        binning=(50, 0, 1000),
+        unit="GeV",
+        x_title=r"$m_{ll,jj}$",
+    )
+    add_variable(
+        config,
+        name="hh_pt",
+        expression=partial(build_hh, which="pt"),
+        aux={"inputs": build_hh.inputs},
+        binning=(40, 0, 400),
+        unit="GeV",
+        x_title=r"$p_{T}_{ll,jj}$",
+    )
+    add_variable(
+        config,
+        name="hh_eta",
+        expression=partial(build_hh, which="eta"),
+        aux={"inputs": build_hh.inputs},
+        binning=(50, -5, 5),
+        unit="GeV",
+        x_title=r"$\eta_{ll,jj}$",
+    )
+    add_variable(
+        config,
+        name="hh_phi",
+        expression=partial(build_hh, which="phi"),
+        aux={"inputs": build_hh.inputs},
+        binning=(32, -3.2, 3.2),
+        unit="GeV",
+        x_title=r"$\phi_{ll,jj}$",
+    )
+    add_variable(
+        config,
+        name="hh_dr",
+        expression=partial(build_hh, which="dr"),
+        aux={"inputs": build_hh.inputs},
+        binning=(30, 0, 6),
+        x_title=r"$\Delta R_{ll,jj}$",
+    )
+
+    # single lepton variables
+    # single electron
+    add_variable(
+        config,
+        name="e1_pt",
+        expression="Electron.pt[:, 0]",
+        binning=(40, 0, 150),
+        x_title=r"Leading electron p$_{T}$",
+    )
+    add_variable(
+        config,
+        name="e2_pt",
+        expression="Electron.pt[:,1]",
+        binning=(30, 0, 150),
+        x_title=r"Subleading electron p_{T}$",
+    )
+    add_variable(
+        config,
+        name="e1_eta",
+        expression="Electron.eta[:,0]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Leading electron \eta$",
+    )
+    add_variable(
+        config,
+        name="e2_eta",
+        expression="Electron.eta[:,1]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Subleading electron \eta$",
+    )
+    add_variable(
+        config,
+        name="e1_phi",
+        expression="Electron.phi[:,0]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Leading electron \phi$",
+    )
+    add_variable(
+        config,
+        name="e2_phi",
+        expression="Electron.phi[:,1]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Subleading electron \phi$",
+    )
+
+    # single tau
+        add_variable(
+        config,
+        name="tau1_pt",
+        expression="Tau.pt[:, 0]",
+        binning=(40, 0, 150),
+        x_title=r"Leading tau p$_{T}$",
+    )
+    add_variable(
+        config,
+        name="tau2_pt",
+        expression="Tau.pt[:,1]",
+        binning=(30, 0, 150),
+        x_title=r"Subleading tau p_{T}$",
+    )
+    add_variable(
+        config,
+        name="tau1_eta",
+        expression="Tau.eta[:,0]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Leading tau \eta$",
+    )
+    add_variable(
+        config,
+        name="tau2_eta",
+        expression="Tau.eta[:,1]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Subleading tau \eta$",
+    )
+    add_variable(
+        config,
+        name="tau1_phi",
+        expression="Tau.phi[:,0]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Leading tau \phi$",
+    )
+    add_variable(
+        config,
+        name="tau2_phi",
+        expression="Tau.phi[:,1]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Subleading tau \phi$",
+    )
+
+    # single mu
+    add_variable(
+        config,
+        name="mu1_pt",
+        expression="Muon.pt[:,0]",
+        binning=(30, 0, 150),
+        x_title=r"Leading muon p_{T}$",
+    )
+    add_variable(
+        config,
+        name="mu2_pt",
+        expression="Muon.pt[:,1]",
+        binning=(30, 0, 150),
+        x_title=r"Subleading muon p_{T}$",
+    )
+    add_variable(
+        config,
+        name="mu1_eta",
+        expression="Muon.eta[:,0]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Leading muon \eta$",
+    )
+    add_variable(
+        config,
+        name="mu2_eta",
+        expression="Muon.eta[:,1]",
+        binning=(25, -2.5, 2.5),
+        x_title=r"Subleading muon \eta$",
+    )
+    add_variable(
+        config,
+        name="mu1_phi",
+        expression="Muon.phi[:,0]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Leading muon \phi$",
+    )
+    add_variable(
+        config,
+        name="mu2_phi",
+        expression="Muon.phi[:,1]",
+        binning=(32, -3.2, 3.2),
+        x_title=r"Subleading muon \phi$",
     )
 
     for proc in ["hh", "tt", "dy"]:
@@ -333,9 +670,10 @@ def add_variables(config: od.Config) -> None:
             x_title=rf"{proc.upper()} output node, res. DNN",
         )
 
-
 # helper to add a variable to the config with some defaults
 def add_variable(config: od.Config, *args, **kwargs) -> od.Variable:
+    kwargs.setdefault("null_value", EMPTY_FLOAT)
+
     # create the variable
     variable = config.add_variable(*args, **kwargs)
 
