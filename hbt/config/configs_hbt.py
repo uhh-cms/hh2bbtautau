b472a068fba752a1bdbab70ed6e35365f311b1ad
Bogdan Wiederspan <b.wiederspan@web.de>
2025-03-06 13:41:33 +0100

    mend

--- hbt/config/configs_hbt.py
+++ hbt/config/configs_hbt.py
@@ -977,18 +977,18 @@ def add_config(
 
     # top pt reweighting
     # https://twiki.cern.ch/twiki/bin/view/CMS/TopPtReweighting?rev=31
-    # from columnflow.production.cms.top_pt_weight import TopPtWeightConfig
-    # cfg.x.top_pt_weight = TopPtWeightConfig(
-    #     params={
-    #         "a": 0.0615,
-    #         "a_up": 0.0615 * 1.5,
-    #         "a_down": 0.0615 * 0.5,
-    #         "b": -0.0005,
-    #         "b_up": -0.0005 * 1.5,
-    #         "b_down": -0.0005 * 0.5,
-    #     },
-    #     pt_max=500.0,
-    # )
+    from columnflow.production.cms.top_pt_weight import TopPtWeightConfig
+    cfg.x.top_pt_weight = TopPtWeightConfig(
+        params={
+            "a": 0.0615,
+            "a_up": 0.0615 * 1.5,
+            "a_down": 0.0615 * 0.5,
+            "b": -0.0005,
+            "b_up": -0.0005 * 1.5,
+            "b_down": -0.0005 * 0.5,
+        },
+        pt_max=500.0,
+    )
 
     ################################################################################################
     # shifts
