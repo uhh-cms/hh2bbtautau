d6327fccea37d05441119f6adfd8014d0af933e8
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2025-04-07 10:29:36 +0200

    add custom early stopping that resets model parameters to best epoch

--- hbt/ml/torch_utils/utils.py
+++ hbt/ml/torch_utils/utils.py
@@ -1,5 +1,14 @@
 from __future__ import annotations
+
+__all__ = [
+    "reorganize_idx",
+]
 from collections import defaultdict
+from columnflow.util import maybe_import, MockModule
+from columnflow.types import Any
+from copy import deepcopy
+
+ignite = maybe_import("ignite")
 
 def reorganize_list_idx(entries):
     first = entries[0]
@@ -28,3 +37,46 @@ def reorganize_idx(batch):
         return reorganize_dict_idx(batch)
     else:
         return reorganize_list_idx(batch)
+    
+if not isinstance(ignite, MockModule):
+    from ignite.handlers import EarlyStopping
+    from ignite.engine import Engine
+    import torch
+
+    class CustomEarlyStopping(EarlyStopping):
+        def __init__(
+            self,
+            *args,
+            model: torch.nn.Module | None = None,
+            min_epochs: int = 1,
+            **kwargs,
+        ):
+            super().__init__(*args, **kwargs)
+            self.best_model: dict[str, Any] | None = None
+            self.min_epochs: int = min_epochs
+            self.model = model or self.trainer._process_function.keywords["model"]
+
+        def __call__(self, engine: Engine) -> None:
+            score = self.score_function(engine)
+
+            if self.best_score is None:
+                self.best_score = score
+            elif score <= self.best_score + self.min_delta:
+                if not self.cumulative_delta and score > self.best_score:
+                    self.best_score = score
+                self.counter += 1
+                self.logger.debug("EarlyStopping: %i / %i" % (self.counter, self.patience))
+                if engine.state.epoch > self.min_epochs and self.counter >= self.patience:
+                    self.logger.info("EarlyStopping: Stop training")
+                    best_epoch = engine.state.epoch - self.patience
+                    self.logger.info(f"Resetting model to epoch {getattr(self.trainer, 'best_epoch', best_epoch)}")
+                    if self.best_model is not None:
+                        self.model.load_state_dict(self.best_model)
+                    else:
+                        self.logger.warning("No best model found, skipping load")
+                    self.trainer.terminate()
+            else:
+                self.best_score = score
+                self.counter = 0
+                self.best_model = deepcopy(self.model.state_dict())
+                setattr(self.trainer, "best_epoch", engine.state.epoch)
