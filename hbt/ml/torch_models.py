05268d50d7e65f36d79b7a5a03672109fe028015
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2025-04-15 14:13:54 +0200

    use new ignite mixins to extend ml model apropriately, first multi class model

--- hbt/ml/torch_models.py
+++ hbt/ml/torch_models.py
@@ -12,6 +12,8 @@ from columnflow.util import MockModule, maybe_import, DotDict
 from columnflow.types import T, Any, Callable, Sequence
 from columnflow.columnar_util import Route, EMPTY_FLOAT, EMPTY_INT
 
+from hbt.ml.torch_utils.functions import get_one_hot
+
 torch = maybe_import("torch")
 torchdata = maybe_import("torchdata")
 np = maybe_import("numpy")
@@ -24,17 +26,40 @@ if not isinstance(torch, MockModule):
     from torch import nn
     from torch.optim import Adam
     import torchdata.nodes as tn
+    from torch.utils.tensorboard import SummaryWriter
+    from ignite.metrics import Loss, ROC_AUC
+
 
     from hbt.ml.torch_utils.datasets import FlatRowgroupParquetDataset, FlatArrowRowGroupParquetDataset
     from hbt.ml.torch_utils.transforms import AkToTensor, PreProcessFloatValues
     from hbt.ml.torch_utils.samplers import ListRowgroupSampler
     from hbt.ml.torch_utils.map_and_collate import NestedListRowgroupMapAndCollate
     from hbt.ml.torch_utils.dataloaders import CompositeDataLoader
-    from hbt.ml.torch_utils.datasets.handlers import FlatListRowgroupParquetFileHandler, FlatArrowParquetFileHandler
+    from hbt.ml.torch_utils.datasets.handlers import (
+        FlatListRowgroupParquetFileHandler, FlatArrowParquetFileHandler,
+        DatasetHandlerMixin,    
+    )
+    from hbt.ml.torch_utils.ignite.mixins import IgniteTrainingMixin, IgniteEarlyStoppingMixin
+    from ignite.metrics.epoch_metric import EpochMetric
 
-    class FeedForwardNet(nn.Module):
-        def __init__(self):
+    class NetworkBase(nn.Module):
+        def __init__(self, *args, tensorboard_path: str | None = None, logger: Any | None = None, **kwargs):
             super().__init__()
+            self.writer = None
+            self.logger = logger or law.logger.get_logger(__name__)
+            if tensorboard_path:
+                self.writer = SummaryWriter(tensorboard_path)
+            self.custom_hooks = list()
+
+
+    class FeedForwardNet(
+        IgniteEarlyStoppingMixin,
+        IgniteTrainingMixin,
+        NetworkBase,
+    ):
+        def __init__(self, *args, tensorboard_path: str | None = None, logger: Any | None = None, task: law.Task, **kwargs):
+            super().__init__(*args, tensorboard_path=tensorboard_path, logger=logger, **task.param_kwargs, **kwargs)
+            
             columns = [
                 "leptons.{px,py,pz,energy,mass}[:, 0]",
                 "leptons.{px,py,pz,energy,mass}[:, 1]",
@@ -59,19 +84,56 @@ if not isinstance(torch, MockModule):
                 nn.Sigmoid(),
             )
 
-            self.loss_fn = nn.BCELoss()
+            self.logger.info(f"Constructing loss and optimizer")
+            self._loss_fn = nn.BCELoss()
+            self.validation_metrics = {
+                "loss": Loss(self.loss_fn),
+                "roc_auc": ROC_AUC(),
+            }
 
-        def init_optimizer(self, learning_rate=1e-3, weight_decay=1e-5):
-            return Adam(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
+        def init_optimizer(self, learning_rate=1e-3, weight_decay=1e-5) -> None:
+            self.optimizer = Adam(self.parameters(), lr=learning_rate, weight_decay=weight_decay)
         
         def _build_categorical_target(self, dataset: str):
             return int(1) if dataset.startswith("hh") else int(0)
-        
+
+        def train_step(self, engine, batch):
+            # Set the model to training mode - important for batch normalization and dropout layers
+            self.train()
+            # Compute prediction and loss
+            X, y = batch[0], batch[1]
+            self.optimizer.zero_grad()
+            pred = self(X)
+            target = y["categorical_target"].to(torch.float32)
+            if target.dim() == 1:
+                target = target.reshape(-1, 1)
+
+            loss = self.loss_fn(pred, target)
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+
+            return loss.item()
+
+        def validation_step(self, engine, batch):
+            # Set the model to evaluation mode - important for batch normalization and dropout layers
+            self.eval()
+
+            # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+            # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+            with torch.no_grad():
+                X, y = batch[0], batch[1]
+                pred = self(X)
+                target = y["categorical_target"].to(torch.float32).reshape(-1, 1)
+                return pred, target
+
         def init_dataset_handler(self, task: law.Task):
             group_datasets = {
                 "ttbar": [d for d in task.datasets if d.startswith("tt_")]
             }
             device = next(self.parameters()).device
+            all_datasets = getattr(task, "resolved_datasets", task.datasets)
+
             self.dataset_handler = FlatListRowgroupParquetFileHandler(
                 task=task,
                 columns=self.inputs,
@@ -80,12 +142,11 @@ if not isinstance(torch, MockModule):
                 build_categorical_target_fn=self._build_categorical_target,
                 group_datasets=group_datasets,
                 device=device,
+                datasets=[d for d in all_datasets if any(d.startswith(x) for x in ["tt_", "hh_"])],
             )
+            self.training_loader, self.validation_loader = self.dataset_handler.init_datasets()            
 
-        def init_datasets(self):
-            return self.dataset_handler.init_datasets()
-        
-        def forward(self, x):
+        def _handle_input(self, x):
             input_data = x
             if isinstance(x, dict):
                 input_data: torch.Tensor = torch.cat([
@@ -97,6 +158,10 @@ if not isinstance(torch, MockModule):
             empty_float = input_data == EMPTY_FLOAT
             empty_int = input_data == EMPTY_INT
             input_data[empty_float | empty_int] = 0
+            return input_data
+
+        def forward(self, x):
+            input_data = self._handle_input(x)
             logits = self.linear_relu_stack(input_data.to(torch.float32))
             return logits
         
@@ -137,8 +202,114 @@ if not isinstance(torch, MockModule):
                 device=device,
             )
 
+
+    class FeedForwardMultiCls(FeedForwardNet):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.linear_relu_stack = nn.Sequential(
+                nn.BatchNorm1d(len(self.inputs),),
+                nn.Linear(len(self.inputs), 512),
+                nn.ReLU(),
+                nn.BatchNorm1d(512),
+                nn.Linear(512, 1024),
+                nn.ReLU(),
+                nn.BatchNorm1d(1024),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+                nn.Linear(512, 3),
+            )
+            self.categorical_target_map = {
+                "hh": 0,
+                "tt": 1,
+                "dy": 2,
+            }
+            self._loss_fn = nn.CrossEntropyLoss()
+            self.validation_metrics = {
+                "loss": Loss(self.loss_fn),
+                # "roc_auc": ROC_AUC(),
+            }
+
+        def validation_step(self, engine, batch):
+            # Set the model to evaluation mode - important for batch normalization and dropout layers
+            self.eval()
+
+            # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+            # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+            with torch.no_grad():
+                X, y = batch[0], batch[1]
+                pred = self(X)
+                target = y["categorical_target"].to(torch.float32).reshape(-1, 1)
+                return torch.nn.functional.softmax(pred), target
+
+        def _build_categorical_target(self, dataset: str):
+            for key in self.categorical_target_map.keys():
+                if dataset.startswith(key):
+                    return self.categorical_target_map[key]
+            raise ValueError(f"Dataset {dataset} not in categorical target map")
+
+        def init_dataset_handler(self, task: law.Task):
+            all_datasets = getattr(task, "resolved_datasets", task.datasets)
+            group_datasets = {
+                "ttbar": [d for d in all_datasets if d.startswith("tt_")],
+                "dy": [d for d in all_datasets if d.startswith("dy_")],
+            }
+            device = next(self.parameters()).device
+
+            self.dataset_handler = FlatListRowgroupParquetFileHandler(
+                task=task,
+                columns=self.inputs,
+                batch_transformations=AkToTensor(device=device),
+                global_transformations=PreProcessFloatValues(),
+                build_categorical_target_fn=self._build_categorical_target,
+                categorical_target_transformation=partial(get_one_hot, nb_classes=3),
+                group_datasets=group_datasets,
+                device=device,
+                datasets=[d for d in all_datasets if any(d.startswith(x) for x in ["tt_", "hh_", "dy_"])],
+            )
+            self.training_loader, self.validation_loader = self.dataset_handler.init_datasets()    
+
+    class DeepFeedForwardMultiCls(FeedForwardMultiCls):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.linear_relu_stack1 = nn.Sequential(
+                nn.BatchNorm1d(len(self.inputs),),
+                nn.Linear(len(self.inputs), 512),
+                nn.ReLU(),
+                nn.BatchNorm1d(512),
+                nn.Linear(512, 1024),
+                nn.ReLU(),
+                nn.BatchNorm1d(1024),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+            )
+            self.linear_relu_stack2 = nn.Sequential(
+                nn.BatchNorm1d(512,),
+                nn.Linear(512, 2048),
+                nn.ReLU(),
+                nn.BatchNorm1d(2048),
+                nn.Linear(2048, 1024),
+                nn.ReLU(),
+                nn.BatchNorm1d(1024),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+                nn.Linear(512, 3),
+            )
+
+            self._loss_fn = nn.CrossEntropyLoss()
+            self.validation_metrics = {
+                "loss": Loss(self.loss_fn),
+                # "roc_auc": ROC_AUC(),
+            }
+
+        def __call__(self, x):
+            input_data = self._handle_input(x)
+            logits = self.linear_relu_stack1(input_data.to(torch.float32))
+            logits = self.linear_relu_stack2(logits)
+            return logits
+
     model_clss["feedforward"] = FeedForwardNet
     model_clss["feedforward_arrow"] = FeedForwardArrow
-
+    model_clss["feedforward_multicls"] = FeedForwardMultiCls
+    model_clss["deepfeedforward_multicls"] = DeepFeedForwardMultiCls
     model_clss["feedforward_dropout"] = DropoutFeedForwardNet
 
