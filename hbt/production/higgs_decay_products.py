78250011dccf5844777581d0d3adff27f40f06f0
Quintus Diepholz <quintus.diepholz@studium.uni-hamburg.de>
2025-04-22 10:09:57 +0200

    Fixed bug in shape_array function (higgs_decay_products)

--- hbt/production/higgs_decay_products.py
+++ hbt/production/higgs_decay_products.py
@@ -15,15 +15,17 @@ from columnflow.columnar_util import attach_coffea_behavior as attach_coffea_beh
 ak = maybe_import("awkward")
 
 
-#Define helper functions
+# Define helper functions
 def shape_array(input_array):
-    output_array = ak.flatten(input_array, axis = 3)
-    output_array = ak.firsts(output_array, axis = 2)
+    output_array = ak.flatten(input_array, axis=3)
+    output_array = ak.flatten(output_array, axis=2)
+    output_array = ak.pad_none(output_array, 2, axis=1)
     return output_array
 
-def add_field(muons, electrons, nu_mu, nu_e, field: str):
-    return ak.fill_none(muons[field][:,0],0) + ak.fill_none(electrons[field][:,0],0) + ak.fill_none(nu_mu[field][:,0],0) + ak.fill_none(nu_e[field][:,0],0)
 
+def add_field(muons, electrons, nu_mu, nu_e, field: str):
+    return ak.fill_none(muons[field][:, 0], 0) + ak.fill_none(electrons[field][:, 0], 0) + ak.fill_none(
+        nu_mu[field][:, 0], 0) + ak.fill_none(nu_e[field][:, 0], 0)
 
 
 @producer(
@@ -32,20 +34,23 @@ def add_field(muons, electrons, nu_mu, nu_e, field: str):
 )
 def higgs_decay_products(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     """
-    Creates a new ragged column "higgs_family" that stores the H and their decay products. The structure will be as follows:
+    Creates a new ragged column "higgs_family" that stores the H and their decay products. The structure will be as
+    follows:
 
     .. code-block:: python
 
         [
             # event 1
             [
-                [H1,H2], [b+, b-], [tau+, tau-], [tau+ nu, tau- nu], [W+, W-] [mu+, mu-], [e+, e-], [mu- nu, mu+ nu], [e- mu, e+ mu]
+                [H_bb, H_tautau], [b+, b-], [tau+, tau-], [tau+ nu, tau- nu], [W+, W-] [mu+, mu-], [e+, e-],
+                [mu- nu, mu+ nu], [e- mu, e+ mu]
             ],
             # event 2
             ...
         ],
 
-    where H1 decays to bb and H2 decays to tautau. If a certain particle is not present in an event, the corresponding array entry will be None. 
+    where H1 decays to bb and H2 decays to tautau. If a certain particle is not present in an event, the
+    corresponding array entry will be None.
     """
 
     mother_gen_flags = ["isLastCopy", "fromHardProcess"]
@@ -55,86 +60,80 @@ def higgs_decay_products(self: Producer, events: ak.Array, **kwargs) -> ak.Array
     abs_id = abs(events.GenPart.pdgId)
     higgs = events.GenPart[abs_id == 25]
     higgs = higgs[higgs.hasFlags(*mother_gen_flags)]
-    higgs_bb = higgs[ak.any(abs(higgs.distinctChildren.pdgId) == 5, axis = -1)]
-    higgs_tautau = higgs[ak.any(abs(higgs.distinctChildren.pdgId) == 15, axis = -1)]
-    higgs_combined = ak.concatenate([higgs_bb, higgs_tautau], axis = 1)
+    higgs_bb = higgs[ak.any(abs(higgs.distinctChildren.pdgId) == 5, axis=-1)]
+    higgs_tautau = higgs[ak.any(abs(higgs.distinctChildren.pdgId) == 15, axis=-1)]
+    higgs_combined = ak.concatenate([higgs_bb, higgs_tautau], axis=1)
 
     higgs_children = higgs.distinctChildren
     higgs_children = higgs_children[higgs_children.hasFlags(*children_gen_flags)]
 
-    #sort different decay products
+    # sort different decay products
     bottoms = higgs_children[abs(higgs_children.pdgId) == 5]
-    bottoms = ak.flatten(bottoms, axis = 2)
-    bottoms = bottoms[ak.argsort(ak.fill_none(bottoms.pdgId, 0), axis = 1, ascending = True)]
+    bottoms = ak.flatten(bottoms, axis=2)
+    bottoms = bottoms[ak.argsort(ak.fill_none(bottoms.pdgId, 0), axis=1, ascending=True)]
 
     taus = higgs_children[abs(higgs_children.pdgId) == 15]
     tau_children = taus.distinctChildrenDeep[taus.distinctChildrenDeep.hasFlags("isFirstCopy")]
-    taus = ak.flatten(taus, axis = 2)
-    taus = taus[ak.argsort(ak.fill_none(taus.pdgId, 0), axis = 1, ascending = True)]
+    taus = ak.flatten(taus, axis=2)
+    taus = taus[ak.argsort(ak.fill_none(taus.pdgId, 0), axis=1, ascending=True)]
 
+    # sort the tau decay products
     nu_tau = shape_array(tau_children[abs(tau_children.pdgId) == 16])
-    nu_tau = nu_tau[ak.argsort(ak.fill_none(nu_tau.pdgId, 0), axis = 1, ascending = True)]
+    nu_tau = nu_tau[ak.argsort(ak.fill_none(nu_tau.pdgId, 0), axis=1, ascending=True)]
     muons = shape_array(tau_children[abs(tau_children.pdgId) == 13])
-    muons = muons[ak.argsort(ak.fill_none(muons.pdgId, 0), axis = 1, ascending = True)]
+    muons = muons[ak.argsort(ak.fill_none(muons.pdgId, 0), axis=1, ascending=True)]
     electrons = shape_array(tau_children[abs(tau_children.pdgId) == 11])
-    electrons = electrons[ak.argsort(ak.fill_none(electrons.pdgId, 0), axis = 1, ascending = True)]
+    electrons = electrons[ak.argsort(ak.fill_none(electrons.pdgId, 0), axis=1, ascending=True)]
     nu_mu = shape_array(tau_children[abs(tau_children.pdgId) == 14])
-    nu_mu = nu_mu[ak.argsort(ak.fill_none(nu_mu.pdgId, 0), axis = 1, ascending = False)]
+    nu_mu = nu_mu[ak.argsort(ak.fill_none(nu_mu.pdgId, 0), axis=1, ascending=False)]
     nu_e = shape_array(tau_children[abs(tau_children.pdgId) == 12])
-    nu_e = nu_e[ak.argsort(ak.fill_none(nu_e.pdgId, 0), axis = 1, ascending = False)]
+    nu_e = nu_e[ak.argsort(ak.fill_none(nu_e.pdgId, 0), axis=1, ascending=False)]
 
-    
     # events = self[attach_coffea_behavior](events, collections = "GenPart", **kwargs)
 
     tau_children_sorted = ak.concatenate([
-        nu_tau[:,None,:],
-        muons[:,None,:],
-        electrons[:,None,:],
-        nu_mu[:,None,:],
-        nu_e[:,None,:]
-    ], axis = 1)
+        nu_tau[:, None, :],
+        muons[:, None, :],
+        electrons[:, None, :],
+        nu_mu[:, None, :],
+        nu_e[:, None, :],
+    ], axis=1)
 
-    behaving_children = attach_coffea_behavior_fn(tau_children_sorted, collections = {"higgs_family":{"type_name": "GenParticle", "check_attr": "metric_table", "skip_fields": "*Idx*G"}})
+    behaving_children = attach_coffea_behavior_fn(tau_children_sorted, collections={"higgs_family": {"type_name":
+        "GenParticle", "check_attr": "metric_table", "skip_fields": "*Idx*G"}})
 
-    W_1 = behaving_children[:,3:,0].sum()
-    W_2 = behaving_children[:,3:,1].sum()
+    W_1 = behaving_children[:, 3:, 0].sum()
+    W_2 = behaving_children[:, 3:, 1].sum()
     W_1["pdgId"] = 24
     W_2["pdgId"] = -24
     W_1["pt"] = W_1.pt
     W_2["pt"] = W_2.pt
-    field_list = np.array(tau_children.fields, dtype = str)
+    field_list = np.array(tau_children.fields, dtype=str)
     for W_iter in [W_1, W_2]:
         for field in field_list:
             if field != "pt" and field != "pdgId":
                 W_iter[field] = None
-    W_bosons = ak.concatenate([W_1[:,None], W_2[:,None]], axis = 1)
+    W_bosons = ak.concatenate([W_1[:, None], W_2[:, None]], axis=1)
     higgs_family = ak.concatenate([
-        higgs_combined[:,None,:],
-        bottoms[:,None,:],
-        taus[:,None,:],
-        W_bosons[:,None,:],
-        tau_children_sorted
-        # nu_tau[:,None,:],
-        # muons[:,None,:],
-        # electrons[:,None,:],
-        # nu_mu[:,None,:],
-        # nu_e[:,None,:]
-    ], axis = 1)
+        higgs_combined[:, None, :],
+        bottoms[:, None, :],
+        taus[:, None, :],
+        W_bosons[:, None, :],
+        tau_children_sorted,
+    ], axis=1)
 
     # save the column
     events = set_ak_column(events, "higgs_family", higgs_family)
 
-
     # for validation, to be removed
-    #save validation columns
-    bottoms_inv_mass = attach_coffea_behavior_fn(bottoms, collections = {"higgs_family":{"type_name": "GenParticle", "check_attr": "metric_table", "skip_fields": "*Idx*G"}}).sum().absolute()
-    taus_inv_mass = attach_coffea_behavior_fn(taus, collections = {"higgs_family":{"type_name": "GenParticle", "check_attr": "metric_table", "skip_fields": "*Idx*G"}}).sum().absolute()
-    events = set_ak_column(events, "bottoms_inv_mass", bottoms_inv_mass)
-    events = set_ak_column(events, "taus_inv_mass", taus_inv_mass)
-    # ak.num(ak.drop_none(ak.flatten(events.higgs_family[:,3:,:].pdgId, axis = 2)), axis = 1)
+    # bottoms_inv_mass = attach_coffea_behavior_fn(bottoms, collections={"higgs_family": {"type_name": "GenParticle",
+    #     "check_attr": "metric_table", "skip_fields": "*Idx*G"}}).sum().absolute()
+    # taus_inv_mass = attach_coffea_behavior_fn(taus, collections={"higgs_family": {"type_name": "GenParticle",
+    #     "check_attr": "metric_table", "skip_fields": "*Idx*G"}}).sum().absolute()
+    # events = set_ak_column(events, "bottoms_inv_mass", bottoms_inv_mass)
+    # events = set_ak_column(events, "taus_inv_mass", taus_inv_mass)
 
-
-    from IPython import embed; embed(header="debugger")
+    # from IPython import embed; embed(header="debugger")
     return events
 
 
