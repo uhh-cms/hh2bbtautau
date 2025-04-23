b0ce146c6fa5421f59d1ec73803e7bafbb153e70
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2025-04-23 14:04:38 +0200

    implement categorical tokenizer by Bogdan Widerspan

--- hbt/ml/torch_utils/utils.py
+++ hbt/ml/torch_utils/utils.py
@@ -5,10 +5,13 @@ __all__ = [
 ]
 from collections import defaultdict
 from columnflow.util import maybe_import, MockModule
+from columnflow.columnar_util import EMPTY_INT
 from columnflow.types import Any
 from copy import deepcopy
 
 ignite = maybe_import("ignite")
+torch = maybe_import("torch")
+np = maybe_import("numpy")
 
 
 def reorganize_list_idx(entries):
@@ -127,3 +130,114 @@ if not isinstance(ignite, MockModule):
                 self.counter = 0
                 self.best_model = deepcopy(self.model.state_dict())
                 setattr(self.trainer, "best_epoch", engine.state.epoch)
+
+
+if not any(isinstance(module, MockModule) for module in (torch, np)):
+    import torch.nn as nn
+
+    def LookUpTable(array: np.ndarray, EMPTY=EMPTY_INT):
+        """Maps multiple categories given in *array* into a sparse vectoriced lookuptable.
+        Empty values are replaced with *EMPTY*.
+
+        Args:
+            array (np.ndarray): 2D array of categories.
+            EMPTY (int, optional): Replacement value if empty. Defaults to columnflow EMPTY_INT.
+
+        Returns:
+            tuple([np.ndarray]): Returns minimum and LookUpTable
+        """
+        # shift input by minimum, pushing the categories to the valid indice space
+        minimum = array.min(dim=0)[0]
+        indice_array = array - minimum
+        upper_bound = torch.max(indice_array) + 1
+
+        # warn for big categories
+        if upper_bound > 100:
+            print("Be aware that a large number of categories will result in a large sparse lookup array")
+
+        # create mapping placeholder
+        mapping_array = torch.full(
+            size=(len(minimum), upper_bound),
+            fill_value=EMPTY,
+            dtype=torch.int32,
+        )
+
+        # fill placeholder with vocabulary
+
+        stride = 0
+        # transpose from event to feature loop
+        for feature_idx, feature in enumerate(indice_array.transpose(0, 1)):
+            unique = torch.unique(feature, dim=None)
+            mapping_array[feature_idx, unique] = torch.arange(
+                stride, stride + len(unique),
+                dtype=torch.int32,
+            )
+            stride += len(unique)
+        return minimum, mapping_array
+
+    class CategoricalTokenizer(nn.Module):
+        def __init__(self, translation: torch.Tensor, minimum: torch.Tensor):
+            """
+            This translaytion layer tokenizes categorical features into a sparse representation.
+            The input tensor is expected to be a 2D tensor with shape (N, M) where N is the number of events and M is the number of features.
+            The output tensor will have shape (N, K) where K is the number of unique categories across all features.
+
+            Args:
+                translation (torch.tensor): Sparse representation of the categories, created by LookUpTable.
+                minimum (torch.tensor): Array of minimum values for each feature. This is necessary to shift the input tensor to the valid index space.
+            """
+            super().__init__()
+            self.map = translation
+            self.min = minimum
+            self.indices = torch.arange(len(minimum))
+
+        def forward(self, x):
+            # shift input array by their respective minimum and slice translation accordingly
+            return self.map[self.indices, x - self.min]
+
+    def test_(print_comparions=False):
+        def prepare_data():
+
+            # unique_values used by the embedding res net
+            # type-str, given input, expected output
+            unique_values = (
+                ("pair_type", [0, 1, 2], [0, 1, 2]),
+                ("decay_mode1", [-1, 0, 1, 10, 11], [3, 4, 5, 6, 7]),
+                ("decay_mode2", [0, 1, 10, 11], [8, 9, 10, 11]),
+                ("charge1", [-1, 1], [12, 13]),
+                ("charge2", [-1, 1], [14, 15]),
+                ("is_boosted", [0, 1], [16, 17]),
+                ("has_jet_pair", [0, 1], [18, 19]),
+                ("spin", [0, 2], [20, 21]),
+                ("year", [0, 1, 2, 3], [22, 23, 24, 25]),
+            )
+
+            data = []
+            expected = []
+            for name, input, output in unique_values:
+                num_cat = len(input)
+                input, output = np.array(input), np.array(output)
+                # sample events randomly (for small events this will break)
+                indices = np.random.choice(num_cat, [1, 10000])
+                data.append(input[indices])
+                expected.append(output[indices])
+
+            data = np.concatenate(data, axis=0).transpose()
+            expected = np.concatenate(expected, axis=0).transpose()
+            return (
+                torch.from_numpy(data)
+                for data in (indices, data, expected)
+            )
+
+        indices, input, expected = prepare_data()
+        min, translation = LookUpTable(input)
+        tokenizer = CategoricalTokenizer(translation, min)
+
+        token = tokenizer(input)
+        match_ = token == expected
+        assert match_.all(axis=None)
+        if print_comparions:
+            for ind in range(len(input)):
+                print(f"Input: {input[ind]}")
+                print(f"Expected: {expected[ind]}")
+                print(f"Tokenized: {token[ind]}")
