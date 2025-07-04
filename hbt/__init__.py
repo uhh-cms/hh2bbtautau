3ba397f0df11259ace0dbe7e91e9391790201a23
Marcel R. <github.riga@icloud.com>
2025-07-04 13:37:58 +0200

    Add env flags.

--- hbt/__init__.py
+++ hbt/__init__.py
@@ -1,5 +1,11 @@
 # coding: utf-8
 
+from __future__ import annotations
+
+__all__ = ["env_is_desy", "env_is_cern"]
+
+import socket
+
 import law
 
 from hbt.columnflow_patches import patch_all
@@ -9,3 +15,10 @@ law.contrib.load("pandas")
 
 # apply cf patches once
 patch_all()
+
+#: Boolean denoting whether the environment is on DESY resources.
+_hostname = socket.gethostname()
+env_is_desy = _hostname.endswith(".desy.de")
+
+#: Boolean denoting whether the environment is on CERN resources.
+env_is_cern = _hostname.endswith(".cern.ch")
