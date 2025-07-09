82ccebbb67fd07b5b953dd667aab018fbb1a7b01
Ana A <ana.rita.alves.andrade@cern.ch>
2025-07-09 17:39:45 +0200

    adding bjet group to control_categories

--- hbt/config/configs_hbt.py
+++ hbt/config/configs_hbt.py
@@ -1619,7 +1619,7 @@ def add_config(
         add_external("dy_recoil_sf", ("/afs/cern.ch/work/m/mrieger/public/mirrors/external_files/Recoil_corrections_v3.json.gz", "v1"))  # noqa: E501
 
         # UHH dy weight
-        add_external("dy_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22pre_v14/prod10_amcatnlo/hbt_corrections_mumu__dyc_eq2j__os_njets-dilep_pt.json.gz", "v1"))  # noqa: E501
+        add_external("dy_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22pre_v14/prod11_dyc/hbt_njets_corrections_njets-dilep_pt.json.gz", "v1"))  # noqa: E501
 
         # trigger scale factors
         trigger_sf_internal_subpath = "AnalysisCore-59ae66c4a39d3e54afad5733895c33b1fb511c47/data/TriggerScaleFactors"
