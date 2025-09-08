ed59d334d48c3251e1b49becd8beb196a76bc119
Ana A <ana.rita.alves.andrade@cern.ch>
2025-09-08 18:33:26 +0200

    typo in DY njet json 2022post

--- hbt/config/configs_hbt.py
+++ hbt/config/configs_hbt.py
@@ -1665,7 +1665,7 @@ def add_config(
                 add_external("dy_njet_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22pre_v14/prod14_dypt/hbt_corrections_njets.json.gz"))  # noqa: E501
             else:
                 add_external("dy_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22post_v14/prod14_nody/hbt_corrections_njets-dilep_pt.json.gz"))  # noqa: E501
-                add_external("dy_njet_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22post_v14/prod14_dypt/hbt_corrections_njets.json"))  # noqa: E501
+                add_external("dy_njet_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/22post_v14/prod14_dypt/hbt_corrections_njets.json.gz"))  # noqa: E501
         elif year == 2023:
             if campaign.has_tag('preBPix'):
                 add_external("dy_weight_sf_uhh", ("/data/dust/user/alvesand/analysis/hh2bbtautau_data/hbt_store/analysis_hbt/hbt.ExportDYWeights/23pre_v14/prod14_nody/hbt_corrections_njets-dilep_pt.json.gz"))  # noqa: E501
