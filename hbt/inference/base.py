b36665a446bd7ea951479d8a8f312abf07eb7c7c
Ana A. <ana.rita.alves.andrade@cern.ch>
2025-09-26 15:28:43 +0200

    resolve merge conflit

--- hbt/inference/base.py
+++ hbt/inference/base.py
@@ -55,21 +55,6 @@ class HBTInferenceModelBase(InferenceModel):
         self.init_parameters()
         self.init_cleanup()
 
-<<<<<<< HEAD
-    def inject_era(self, config_inst: od.Config, combine_name: str) -> str:
-        # helper to inject era info into combine process names
-        campaign_key = self.campaign_keys[config_inst]
-        # for HH, inject the key before the ecm value
-        if (m := re.match(r"^((ggHH|qqHH)_.+_13p(0|6)TeV)_(hbbhtt)$", combine_name)):
-            return f"{m.group(1)}_{campaign_key}_{m.group(4)}"
-        # for single H, inject the key before the higgs decay
-        if (m := re.match(r"^(.+)_(hbb|htt)$", combine_name)):
-            return f"{m.group(1)}_{campaign_key}_{m.group(2)}"
-        # for all other processes, just append the campaign key
-        return f"{combine_name}_{campaign_key}"
-
-=======
->>>>>>> master
     def init_objects(self) -> None:
         # gather campaign identifier keys per config
         self.single_config = len(self.config_insts) == 1
