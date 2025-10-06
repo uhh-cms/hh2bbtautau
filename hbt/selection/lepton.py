8ece14fccbd9a87f19118b47bf1e0218a2c6edaf
Marcel R. <github.riga@icloud.com>
2025-10-06 12:46:29 +0200

    Use fill_none to remove none's in selection masks.

--- hbt/selection/lepton.py
+++ hbt/selection/lepton.py
@@ -996,9 +996,9 @@ def lepton_selection(
     events = set_ak_column(events, "matched_trigger_ids", matched_trigger_ids)
 
     # remove option types
-    sel_electron_mask = ak.drop_none(sel_electron_mask)
-    sel_muon_mask = ak.drop_none(sel_muon_mask)
-    sel_tau_mask = ak.drop_none(sel_tau_mask)
+    sel_electron_mask = ak.fill_none(sel_electron_mask, False)
+    sel_muon_mask = ak.fill_none(sel_muon_mask, False)
+    sel_tau_mask = ak.fill_none(sel_tau_mask, False)
 
     # convert lepton masks to sorted indices (pt for e/mu, iso for tau)
     sel_electron_indices = sorted_indices_from_mask(sel_electron_mask, events.Electron.pt, ascending=False)
