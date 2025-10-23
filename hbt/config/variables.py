60058a25bf1d0fc82a0fe8eb45419955957976dd
Ana A. <ana.rita.alves.andrade@cern.ch>
2025-10-23 18:52:49 +0200

    remove duplicate

--- hbt/config/variables.py
+++ hbt/config/variables.py
@@ -960,14 +960,6 @@ def add_variables(config: od.Config) -> None:
 
         add_variable(
             config,
-            name=f"run3_dnn_moe_{proc}_fine",
-            expression=f"run3_dnn_moe_{proc}",
-            binning=(5000, 0.0, 1.0),
-            x_title=rf"DNN {proc.upper()} output node",
-        )
-
-        add_variable(
-            config,
             name=f"run3_dnn_moe_{proc}",
             expression=f"run3_dnn_moe_{proc}",
             binning=(25, 0.0, 1.0),
