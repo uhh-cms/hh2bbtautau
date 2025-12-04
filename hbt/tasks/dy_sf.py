fcc7c575eb24b3d8ba34af4bea27ef2f3cd2fcfa
Ana A. <ana.rita.alves.andrade@cern.ch>
2025-12-04 16:56:36 +0100

    fix hist import

--- hbt/tasks/dy_sf.py
+++ hbt/tasks/dy_sf.py
@@ -17,8 +17,8 @@ import correctionlib.schemav2 as cs
 
 from scipy import optimize
 from matplotlib import pyplot as plt
-from typing import Callable
 
+from columnflow.types import TYPE_CHECKING, Callable
 from columnflow.hist_util import create_hist_from_variables, fill_hist
 from hbt.tasks.base import HBTTask
 from columnflow.tasks.framework.base import TaskShifts, ConfigTask
@@ -34,7 +34,8 @@ from columnflow.tasks.framework.mixins import (
     SelectorClassMixin, ReducerClassMixin,
 )
 
-hist = maybe_import("hist")
+if TYPE_CHECKING:
+    hist = maybe_import("hist")
 
 
 @dataclasses.dataclass
@@ -539,11 +540,11 @@ class DYWeights(DYBaseTask):
         return h
 
     def get_ratio_values(
-            self,
-            data_h: hist.Hist,
-            dy_h: hist.Hist,
-            bkg_h: hist.Hist,
-            variable_inst: od.Variable,
+        self,
+        data_h: hist.Hist,
+        dy_h: hist.Hist,
+        bkg_h: hist.Hist,
+        variable_inst: od.Variable,
     ) -> tuple[hist.Hist, hist.Hist, hist.Hist]:
 
         # under/overflow treatment
