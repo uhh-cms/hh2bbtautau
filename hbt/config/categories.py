a6bba193b1d398241956819b9102b1affd90cc4d
rapha <raphael.guitton@etu.unistra.fr>
2025-12-11 16:12:53 +0100

    cleaning

--- hbt/config/categories.py
+++ hbt/config/categories.py
@@ -18,6 +18,135 @@ from columnflow.types import Any
 logger = law.logger.get_logger(__name__)
 
 
+def add_categories_2024(config: od.Config) -> None:
+    """
+    Adds all categories to a *config*.
+    """
+    # root category (-1 has special meaning in cutflow)
+    root_cat = add_category(config, name="all", id=-1, selection="cat_all", label="")
+    _add_category = functools.partial(add_category, config, parent=root_cat)
+
+    # reset the maximum category id counter
+    od.Category._max_id = 0
+
+    # lepton channels
+    _add_category(name="etau", id=1, selection="cat_etau", label=config.channels.n.etau.label)
+    _add_category(name="mutau", id=2, selection="cat_mutau", label=config.channels.n.mutau.label)
+    _add_category(name="tautau", id=3, selection="cat_tautau", label=config.channels.n.tautau.label)
+    _add_category(name="ee", id=4, selection="cat_ee", label=config.channels.n.ee.label)
+    _add_category(name="mumu", id=5, selection="cat_mumu", label=config.channels.n.mumu.label)
+    _add_category(name="emu", id=6, selection="cat_emu", label=config.channels.n.emu.label)
+
+    # qcd regions
+    _add_category(name="os", id="+", selection="cat_os", label="OS", tags={"os"})
+    _add_category(name="ss", id="+", selection="cat_ss", label="SS", tags={"ss"})
+    _add_category(name="iso", id="+", selection="cat_iso", label=r"iso", tags={"iso"})
+    _add_category(name="noniso", id="+", selection="cat_noniso", label=r"non-iso", tags={"noniso"})  # noqa: E501
+
+    # kinematic categories
+    _add_category(name="incl", id="+", selection="cat_incl", label="inclusive")
+
+    _add_category(name="dy", id="+", selection="cat_dy", label="DY enriched")
+    _add_category(name="dyc", id="+", selection="cat_dyc", label="DY region")
+    _add_category(name="dy_st", id="+", selection=["cat_dy", "cat_single_triggered"], label="DY enriched, ST")
+    _add_category(name="tt", id="+", selection="cat_tt", label=r"$t\bar{t}$ enriched")
+    _add_category(name="mll40", id="+", selection="cat_mll40", label=r"$m_{ll} > 40$")
+
+    def name_fn(categories: dict[str, od.Category]) -> str:
+        return "__".join(cat.name for cat in categories.values() if cat)
+
+    def kwargs_fn(categories: dict[str, od.Category], add_qcd_group: bool = True) -> dict[str, Any]:
+        # build auxiliary information
+        aux = {}
+        if add_qcd_group:
+            aux["qcd_group"] = name_fn({
+                name: cat for name, cat in categories.items()
+                if name not in {"sign", "tau2"}
+            })
+        # return the desired kwargs
+        return {
+            # just increment the category id
+            # NOTE: for this to be deterministic, the order of the categories must no change!
+            "id": "+",
+            # join all tags
+            "tags": set.union(*[cat.tags for cat in categories.values() if cat]),
+            # auxiliary information
+            "aux": aux,
+            # label
+            "label": ", ".join([
+                cat.label
+                for cat in categories.values()
+                if cat.label and cat.name != "os" and cat.name != "iso"  # os and iso are the defaults
+            ]) or None,
+        }
+
+    # main analysis categories
+    main_categories = {
+        # channels first
+        "channel": CategoryGroup(["etau", "mutau", "tautau"], is_complete=False, has_overlap=False),
+        # kinematic regions in the middle (to be extended)
+        "kin": CategoryGroup(["incl"], is_complete=True, has_overlap=True),  # noqa: E501
+        # qcd regions last
+        "sign": CategoryGroup(["os", "ss"], is_complete=True, has_overlap=False),
+        "tau2": CategoryGroup(["iso", "noniso"], is_complete=True, has_overlap=False),
+    }
+
+    create_category_combinations(
+        config=config,
+        categories=main_categories,
+        name_fn=name_fn,
+        parent_mode="safe",
+        kwargs_fn=functools.partial(kwargs_fn, add_qcd_group=True),
+        skip_existing=False,
+    )
+
+    # control categories
+    control_categories = {
+        # channels
+        "channel": CategoryGroup(["ee", "mumu", "emu"], is_complete=False, has_overlap=False),
+        # kinematic regions
+        "kin": CategoryGroup(["incl", "dy", "dyc", "tt", "dy_st", "mll40"], is_complete=True, has_overlap=True),
+        # relative sign
+        "sign": CategoryGroup(["os"], is_complete=False, has_overlap=False),
+    }
+
+    def skip_fn_ctrl(categories: dict[str, od.Category]) -> bool:
+        if "channel" not in categories or "kin" not in categories:
+            return False
+        ch_cat = categories["channel"]
+        kin_cat = categories["kin"]
+        # skip dy in emu
+        if kin_cat.name.startswith("dy") and ch_cat.name == "emu":
+            return True
+        # skip tt in ee/mumu
+        if kin_cat.name == "tt" and ch_cat.name in {"ee", "mumu"}:
+            return True
+        return False
+
+    create_category_combinations(
+        config=config,
+        categories=control_categories,
+        name_fn=name_fn,
+        parent_mode="safe",
+        kwargs_fn=functools.partial(kwargs_fn, add_qcd_group=False),
+        skip_existing=False,
+        skip_fn=skip_fn_ctrl,
+    )
+
+    # additional combined categories
+    _add_category(
+        name="incl__os__iso",
+        id="+",
+        label=r"$e\tau/\mu\tau/\tau\tau$, inclusive",
+        categories=[
+            config.get_category("etau__incl__os__iso"),
+            config.get_category("mutau__incl__os__iso"),
+            config.get_category("tautau__incl__os__iso"),
+        ],
+    )
+    track_category_changes(config)
+
+
 def add_categories(config: od.Config) -> None:
     """
     Adds all categories to a *config*.
