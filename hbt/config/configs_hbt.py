168ab99936078dfbf76b08dbbda97f7321846a01
Marcel R. <github.riga@icloud.com>
2026-03-12 16:29:45 +0100

    Update cf.

--- hbt/config/configs_hbt.py
+++ hbt/config/configs_hbt.py
@@ -1966,14 +1966,6 @@ def add_config(
         cclub_hash, cclub_branch = "c3441320", "cclub_cmssw15010"
         vbfnn_postfix = "_24" if year == 2024 else "_22-23"
         # TODO: misses th TF version, so added old v5 model below for now
-        # add_external("vbf_dnn_repo", Ext(
-        #     f"{central_hbt_dir}/AnalysisCore-{cclub_hash}.tar.gz",
-        #     subpaths=DotDict.wrap({
-        #         f"fold{f}": f"AnalysisCore-{cclub_branch}/data/DNN_models/HHRun3DNN/vbf_model_v6{vbfnn_postfix}/model_{f}"  # noqa: E501
-        #         for f in range(5)
-        #     }),
-        #     version="v6",
-        # ))
         add_external("vbf_dnn_repo", Ext(
             f"{central_hbt_dir}/AnalysisCore-f69dda6c.tar.gz",
             subpaths=DotDict.wrap({
@@ -1982,6 +1974,14 @@ def add_config(
             }),
             version="v5",
         ))
+        # add_external("vbf_dnn_repo", Ext(
+        #     f"{central_hbt_dir}/AnalysisCore-{cclub_hash}.tar.gz",
+        #     subpaths=DotDict.wrap({
+        #         f"fold{f}": f"AnalysisCore-{cclub_branch}/data/DNN_models/HHRun3DNN/vbf_model_v6{vbfnn_postfix}/model_{f}"  # noqa: E501
+        #         for f in range(5)
+        #     }),
+        #     version="v6",
+        # ))
         # muon energy (scale and resolution) corrections and helper tools
         add_external("muon_sr", (cat_info.get_file("muo", "muon_scalesmearing.json.gz"), "v1"))
         add_external("muon_sr_tools", Ext(
